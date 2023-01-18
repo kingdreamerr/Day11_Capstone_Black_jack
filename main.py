f178f48c4604daa8cd9d07c8fc86497799865f84
kingdreamerr <jackmvula458@gmail.com>
2023-01-18 19:39:24 +0200

    Add calculate function

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
+
+
+def calculate(cards):
+  """Inputs a list and returns a sum of the list"""
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  
+  return sum(cards)
